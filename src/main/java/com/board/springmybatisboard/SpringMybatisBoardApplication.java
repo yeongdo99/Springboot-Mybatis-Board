1ec0b3bd49f5b06aea115d5273c69fa26e0c0f32
yeongdo99 <atpsfka1@gmail.com>
2024-04-17 14:29:14 +0900

    First Commit

---
+++ src/main/java/com/board/springmybatisboard/SpringMybatisBoardApplication.java
@@ -0,0 +1,13 @@
+package com.board.springmybatisboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMybatisBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMybatisBoardApplication.class, args);
+    }
+
+}
